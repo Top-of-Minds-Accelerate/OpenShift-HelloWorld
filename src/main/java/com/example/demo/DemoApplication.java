46c06403060aaf9c348380c0a7fa205ded9e9cc4
mikael-lennehag <54863295+mikael-lennehag@users.noreply.github.com>
2023-02-14 21:46:28 +0100

    Missing imports stupid - Lenny

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @RestController
