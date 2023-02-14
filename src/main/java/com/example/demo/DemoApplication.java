75f61792d0d70e4659e9c1850a52854c0cdb4d42
mikael-lennehag <mikael.lennehag@topofminds.se>
2023-02-14 20:47:50 +0100

    first commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@SpringBootApplication
+public class DemoApplication {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World HAAANK";
+    }
+
+    @RequestMapping("/live")
+    String live() {
+        return "ok";
+    }
+    
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
